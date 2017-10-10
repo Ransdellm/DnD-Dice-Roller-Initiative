57e79b1ece91c7cfd346c164505cec889745bc07
Matthew R <Ransdellm@gmail.com>
2017-10-09 21:41:27 -0400

    added ES6 notation
    Changed functions within js file to instead use ES6 arrow functions and
    removed all uses of var. Instead used let and const

--- dice.js
+++ dice.js
@@ -1,39 +1,39 @@
-// calls d6 function when button is clicked
-document.getElementById("d4").addEventListener("click", rollD4);
-document.getElementById("d6").addEventListener("click", rollD6);
-document.getElementById("d8").addEventListener("click", rollD8);
-document.getElementById("d10").addEventListener("click", rollD10);
-document.getElementById("d12").addEventListener("click", rollD12);
-document.getElementById("d20").addEventListener("click", rollD20);
-document.getElementById("d100").addEventListener("click", rollD100);
-
-
-// function that rolls d6
-function rollD4 (){
-  var randomD4 = Math.floor((Math.random() * 4 ) + 1);
+// calls dice roll function when button is clicked
+window.onload = function () {
+document.getElementById("rollD4").onclick = rollD4;
+document.getElementById("rollD6").onclick = rollD6;
+document.getElementById("rollD8").onclick = rollD8;
+document.getElementById("rollD10").onclick = rollD10;
+document.getElementById("rollD12").onclick = rollD12;
+document.getElementById("rollD20").onclick = rollD20;
+document.getElementById("rollD100").onclick = rollD100;
+}
+// function that rolls dice
+rollD4 = () => {
+  const randomD4 = Math.floor((Math.random() * 4 ) + 1);
 alert(randomD4);
 }
-function rollD6 (){
-  var randomD6 = Math.floor((Math.random() * 6 ) + 1);
+rollD6 = () => {
+  const randomD6 = Math.floor((Math.random() * 6 ) + 1);
 alert(randomD6);
 }
-function rollD8 (){
-  var randomD8 = Math.floor((Math.random() * 8 ) + 1);
+rollD8 = () => {
+  const randomD8 = Math.floor((Math.random() * 8 ) + 1);
 alert(randomD8);
 }
-function rollD10 (){
-  var randomD10 = Math.floor((Math.random() * 10 ) + 1);
+rollD10 = () => {
+  const randomD10 = Math.floor((Math.random() * 10 ) + 1);
 alert(randomD10);
 }
-function rollD12 (){
-  var randomD12 = Math.floor((Math.random() * 12 ) + 1);
+rollD12 = () => {
+  const randomD12 = Math.floor((Math.random() * 12 ) + 1);
 alert(randomD12);
 }
-function rollD20 (){
-  var randomD20 = Math.floor((Math.random() * 20 ) + 1);
+rollD20 = () => {
+  const randomD20 = Math.floor((Math.random() * 20 ) + 1);
 alert(randomD20);
 }
-function rollD100 (){
-  var randomD100 = Math.floor((Math.random() * 100 ) + 1);
+rollD100 = () => {
+  const randomD100 = Math.floor((Math.random() * 100 ) + 1);
 alert(randomD100);
 }
